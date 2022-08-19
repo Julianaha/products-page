28c78de862ebb0e588c14e4a8b019971637c2e43
Juliana <julianaha01@gmail.com>
2022-08-19 09:26:02 -0300

    fix: altera css modules no arquivo form

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,21 +1,21 @@
-import "./style.css";
+import styles from "./Formulario.module.css";
 
 function Formulario() {
   return (
     <form>
       <label>Seu nome:</label>
-      <input className="styleInput" />
+      <input className={styles.styleInput} />
       <label>E-mail</label>
-      <input  className="styleInput" />
+      <input className={styles.styleInput} />
       <label>CPF:</label>
-      <input className="styleInput" />
-      <div className="styleRadio">
+      <input className={styles.styleInput} />
+      <div className={styles.styleRadio}>
         <input type="radio" id="men" name="masculino" value="masculino" />
         <label htmlFor="masculino">Masculino</label>
         <input type="radio" id="woman" name="feminino" value="feminino" />
         <label htmlFor="feminino">Feminino</label>
       </div>
-      {/* <button className="button-form">Enviar</button> */}
+      <button className={styles.btnForm}>Enviar</button>
     </form>
   );
 }
