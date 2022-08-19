9c0425f4a47db7e30e1dd986be58fe5acf0083b7
Juliana <julianaha01@gmail.com>
2022-08-19 09:06:55 -0300

    fix: altera para css modules

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,23 @@
-import "./style.css";
+import styles from "./Header.module.css";
 import { Menu } from "../Menu";
 
 export function Header() {
   return (
-    <div className="background-header">
-    <header className="header-flex container">
-      <p className="header-title">uma seleção de produtos</p>
-      <p className="header-focus">especial para você</p>
-      <p className="header-p">
-        Todos os produtos desta lista foram selecionado a partir da sua
-        navegação. Aproveite!
-      </p>
-      <div className="menu-button">
-        <Menu name="Conheça a Linx" />
-        <Menu name="Ajude o algorítimo" />
-        <Menu name="Seus produtos" />
-        <Menu name="Compartilhe" />
-      </div>
-    </header>
-
+    <div className={styles.backgroundHeader}>
+      <header className={styles.headerFlex}>
+        <p className={styles.headerTitle}>uma seleção de produtos</p>
+        <p className={styles.headerFocus}>especial para você</p>
+        <p className={styles.header_p}>
+          Todos os produtos desta lista foram selecionado a partir da sua
+          navegação. Aproveite!
+        </p>
+        <div className={styles.menuButton}>
+          <Menu name="Conheça a Linx" />
+          <Menu name="Ajude o algorítimo" />
+          <Menu name="Seus produtos" />
+          <Menu name="Compartilhe" />
+        </div>
+      </header>
     </div>
   );
 }
